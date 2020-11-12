7f26a632eea43dd2ae3f74e29dbb5d753e758eb7
parkchoongho <pchyo92@gmail.com>
2020-11-12 22:25:10 +0900

    Ch8 공부

---
+++ ch8-exceptional-control-flow/fork.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void unix_error(char *msg){
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+  exit(0);
+}
+
+pid_t Fork(void){
+  pid_t pid;
+  
+  if((pid = fork()) < 0)
+    unix_error("Fork error");
+  return pid;
+}
+
+int main(){
+  pid_t pid;
+  int x = 1;
+
+  pid = Fork();
+  if(pid == 0){ /* Child */
+    printf("child : x = %d\n", ++x);
+    exit(0);  
+  }
+
+  /* Parent */
+  printf("parent : x = %d\n", --x);
+  exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
